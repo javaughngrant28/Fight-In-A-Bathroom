8df274149087321d87939388632ae994a64b1128
Grant <javaughngrant28@gmail.com>
2025-09-09 19:18:38 -0500

    Send Grabbed Player To Server

--- src/client/Controllers/Input/Grab/init.lua
+++ src/client/Controllers/Input/Grab/init.lua
@@ -6,18 +6,38 @@ local GrabAnimation = require(script.GrabAnimation)
 local CombatCooldowns = require(game.ReplicatedStorage.Shared.Data.Combat.CombatCooldowns)
 local StateEnum = require(game.ReplicatedStorage.Shared.Data.Character.StateEnum)
 local CombatEnum = require(game.ReplicatedStorage.Shared.Data.Combat.CombatEnum)
+local Hitbox = require(script.Parent.Parent.Parent.Modules.Hitbox)
+
 
 local INDEX = CombatEnum.Grab
+
+local function FilterFunction(target: Model): boolean
+    local state = target:GetAttribute(StateEnum.ATTRIBUTE_NAME)
+    return state == nil or state ~= StateEnum.Downed
+end
+
+local function FireHitDetection(remote: RemoteEvent)
+
+    local hitboxInfo: Hitbox.HitboxInfo = {
+        Character = Player.Character,
+        Filter = FilterFunction,
+    }
+    
+    local results = Hitbox.Fire(hitboxInfo)
+    local target = results['Target'] or nil
+
+    remote:FireServer(INDEX,target)
+end
 
 
 local function GrabRequest(remote: RemoteEvent)
     local state = Player.Character:GetAttribute(StateEnum.ATTRIBUTE_NAME)
     local grabCooldownAttribute = Player:GetAttribute(CombatCooldowns.Grab.ATTRIBUTE_NAME)
-
+    
     if state ~= StateEnum.None then return end
     if grabCooldownAttribute and grabCooldownAttribute == true then return end
-
-    remote:FireServer(INDEX)
+    
+    FireHitDetection(remote)
     GrabAnimation.Play()
 end
 
